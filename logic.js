451f34ced30ce2614d035f6dc9ab92dc2461d5bc
Allegra Adinolfi <allegraadinolfi@Allegras-MacBook.local>
2018-04-05 11:54:02 -0700

    some changes

--- logic.js
+++ logic.js
@@ -6,38 +6,51 @@ d3.json(eartquake_url, function(data){
 });
 
 function createFeatures(eartquake_data) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.mag +
+          "</h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+      }
     var eartquake = L.geoJSON(eartquake_data, {
-        onEachFeature: function(features, layer){
-            layer.binfPopup("<h3>" + features.properties.mag + "</h3>" 
-            "<h3>" + features.properties.place + "</h3><hr><p>" + new Date(feature.properties.time)+ "</p>");
-        },
-        pointToLayer: function (feature, latlng) {
+        onEachFeature: onEachFeature,
+        pointToLayer: function(feature, latlng){
             return new L.circle(latlng,
-              {radius: getRadius(feature.properties.mag),
-              fillColor: getColor(feature.properties.mag),
-              fillOpacity: .6,
-              color: "#000",
-              stroke: true,
-              weight: .8
-          })
+            {fillColor: "red",
+            wight: 0.1,
+            fillOpacity: 0.8,
+            color: "black"
+        })
         }
-    });
+        });
+        // pointToLayer: function (feature, latlng) {
+    //         return new L.circle(latlng,
+    //           {radius: 8,
+    //           fillColor: ,
+    //           fillOpacity: .6,
+    //           color: "#000",
+    //           stroke: true,
+    //           weight: .8
+    //       })
+    //     }
+    // });
     createMap(eartquake);
 }
 
+
+
 function createMap(earthquake){
       // Define streetmap and darkmap layers
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-    "T6YbdDixkOBWH_k9GbS8JQ");
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-    "T6YbdDixkOBWH_k9GbS8JQ");
+    // var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRpbm9sZmlhbGxlZ3JhIiwiYSI6ImNqZXZvcHU5YTBsNzUydnBzOTRtZm43Y20ifQ.13CyjP5TsBxoBDhfNFGCuQ");
+    // var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRpbm9sZmlhbGxlZ3JhIiwiYSI6ImNqZXZvcHU5YTBsNzUydnBzOTRtZm43Y20ifQ.13CyjP5TsBxoBDhfNFGCuQ");
+    // var gray = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRpbm9sZmlhbGxlZ3JhIiwiYSI6ImNqZXZvcHU5YTBsNzUydnBzOTRtZm43Y20ifQ.13CyjP5TsBxoBDhfNFGCuQ");
+    var satelliteStreet = L.tileLayer("https://api.mapbox.com/styles/v1/abdullah1982/cjfkmkifh61fv2spbe9vt63b5/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWJkdWxsYWgxOTgyIiwiYSI6ImNqZXZvcW02bDBsOXIzNW43NjF4Y3U3N3AifQ.ERPS9G84Dkk0buz91kG3FA");
+
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/abdullah1982/cjfkmpd9s04ck2rn4qtpb0cah/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWJkdWxsYWgxOTgyIiwiYSI6ImNqZXZvcW02bDBsOXIzNW43NjF4Y3U3N3AifQ.ERPS9G84Dkk0buz91kG3FA");
+
     var gray = L.tileLayer("https://api.mapbox.com/styles/v1/abdullah1982/cjfla3gwz05m52so2en7hf6al/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWJkdWxsYWgxOTgyIiwiYSI6ImNqZXZvcW02bDBsOXIzNW43NjF4Y3U3N3AifQ.ERPS9G84Dkk0buz91kG3FA");
   
     // defining the base maps layers and pass in other layers 
     var baseMaps = {
-      "Satellite Street": streetmap,
+      "Satellite Street": satelliteStreet,
       "Dark Map": darkmap,
       "Gray Scale": gray
     };
@@ -50,7 +63,7 @@ function createMap(earthquake){
             37.09, 95.71
         ],
         zoom: 2.5,
-        layers: [streetmap, earthquake]
+        layers: [satelliteStreet, earthquake]
         
     });
 
