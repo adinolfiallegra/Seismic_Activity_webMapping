3f6a0bd660b9b29ca56a4f9922ea5f82bc2bb660
Allegra Adinolfi <allegraadinolfi@Allegras-MacBook.local>
2018-04-07 09:44:55 -0700

    checking last items

--- logic.js
+++ logic.js
@@ -1,47 +1,42 @@
-var eartquake_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson';
+
+var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
+
 var tectonicPlatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-d3.json(eartquake_url, function(data){
-    createFeatures(data.features);
+// quering urls
+d3.json(earthquakeURL, function(data) {
+  createFeatures(data.features);
 });
 
-function createFeatures(eartquake_data) {
-    function onEachFeature(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.mag +
-          "</h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-      }
-    var eartquake = L.geoJSON(eartquake_data, {
-        onEachFeature: onEachFeature,
-        pointToLayer: function(feature, latlng){
-            return new L.circle(latlng,
-            {fillColor: "red",
-            wight: 0.1,
-            fillOpacity: 0.8,
-            color: "black"
-        })
-        }
-        });
-        // pointToLayer: function (feature, latlng) {
-    //         return new L.circle(latlng,
-    //           {radius: 8,
-    //           fillColor: ,
-    //           fillOpacity: .6,
-    //           color: "#000",
-    //           stroke: true,
-    //           weight: .8
-    //       })
-    //     }
-    // });
-    createMap(eartquake);
+function createFeatures(earthquakeData) {
+
+  
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +"</h3><h3>Location: "+ feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    },
+    
+    pointToLayer: function (feature, latlng) {
+      return new L.circle(latlng,
+        {radius: getRadius(feature.properties.mag),
+        fillColor: getColor(feature.properties.mag),
+        fillOpacity: .6,
+        color: "#000",
+        stroke: true,
+        weight: .8
+    })
+  }
+  });
+
+  createMap(earthquakes);
 }
 
 
+function createMap(earthquakes) {
 
-function createMap(earthquake){
-      // Define streetmap and darkmap layers
-    // var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRpbm9sZmlhbGxlZ3JhIiwiYSI6ImNqZXZvcHU5YTBsNzUydnBzOTRtZm43Y20ifQ.13CyjP5TsBxoBDhfNFGCuQ");
-    // var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRpbm9sZmlhbGxlZ3JhIiwiYSI6ImNqZXZvcHU5YTBsNzUydnBzOTRtZm43Y20ifQ.13CyjP5TsBxoBDhfNFGCuQ");
-    // var gray = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWRpbm9sZmlhbGxlZ3JhIiwiYSI6ImNqZXZvcHU5YTBsNzUydnBzOTRtZm43Y20ifQ.13CyjP5TsBxoBDhfNFGCuQ");
+    // defining maps layers
+    
     var satelliteStreet = L.tileLayer("https://api.mapbox.com/styles/v1/abdullah1982/cjfkmkifh61fv2spbe9vt63b5/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWJkdWxsYWgxOTgyIiwiYSI6ImNqZXZvcW02bDBsOXIzNW43NjF4Y3U3N3AifQ.ERPS9G84Dkk0buz91kG3FA");
 
     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/abdullah1982/cjfkmpd9s04ck2rn4qtpb0cah/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWJkdWxsYWgxOTgyIiwiYSI6ImNqZXZvcW02bDBsOXIzNW43NjF4Y3U3N3AifQ.ERPS9G84Dkk0buz91kG3FA");
@@ -54,47 +49,68 @@ function createMap(earthquake){
       "Dark Map": darkmap,
       "Gray Scale": gray
     };
+
+
+    var tectonicPlates = new L.LayerGroup();
+
     var overlayMaps = {
-        Earthquakes: earthquake
+      "Earthquakes": earthquakes,
+      "Tectonic Plates": tectonicPlates
     };
 
+    // setting the center of the map and zoom
     var myMap = L.map("map", {
-        center:[
-            37.09, 95.71
-        ],
-        zoom: 2.5,
-        layers: [satelliteStreet, earthquake]
-        
-    });
+      center: [
+        21.3891, 39.8579],
+      zoom: 2.25,
+      layers: [satelliteStreet, earthquakes,]
+    }); 
 
-//     // tectonic plates layer
-//     var tectonicPlates = new L.LayerGroup();
-
-//     var overlayMaps = {
-//       "Earthquakes": earthquake,
-//       "Tectonic Plates": tectonicPlates
-//     };
-
-//     // setting the center of the map and zoom
-//     var myMap = L.map("map", {
-//       center: [
-//         21.3891, 39.8579],
-//       zoom: 2.25,
-//       layers: [streetmap, earthquake, tectonicPlates]
-//     }); 
-
-//     // fault lines data, geoJSON data, style information
-//     d3.json(tectonicPlatesURL, function(plateData) {
-//       L.geoJson(plateData, {
-//         color: "yellow",
-//         weight: 2
-//       })
-//       .addTo(tectonicPlates);
-//   });
+    d3.json(tectonicPlatesURL, function(plateData) {
+      L.geoJson(plateData, {
+        color: "yellow",
+        weight: 2
+      })
+      .addTo(tectonicPlates);
+  });
 
   
-    // control layer
-    L.control.layers(baseMaps, overlayMaps, {
+
+    L.control.layers(baseMaps, {
       collapsed: false
     }).addTo(myMap);
+
+
+  var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function(myMap){
+      var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+  // looping density
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+
+  legend.addTo(myMap);
 }
+ 
+ 
+  function getColor(d){
+    return d > 5 ? "darkred":
+    d  > 4 ? "darkorange":
+    d > 3 ? "orange":
+    d > 2 ? "yellow":
+    d > 1 ? "yellowgreen":
+             "green";
+  }
+
+  // maginutde factor of 45,000 for circles. 
+  function getRadius(value){
+    return value*45000
+  }
